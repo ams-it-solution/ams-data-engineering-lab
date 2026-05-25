ffe9df5b0209a16458067af6bb042972dea5ded4
Andre Moraes <andre.189b@gmail.com>
2026-05-24 23:51:34 -0300

    Refactor environment variables and improve project security

--- spark/spark_jobs/sales_etl.py
+++ spark/spark_jobs/sales_etl.py
@@ -1,35 +1,47 @@
 import os
+
 os.environ["PYSPARK_SUBMIT_ARGS"] = "--packages org.postgresql:postgresql:42.7.3 pyspark-shell"
 
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, sum as _sum
 
+# Environment variables
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+
+# JDBC URL
+jdbc_url = f"jdbc:postgresql://{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+# Spark session
 spark = SparkSession.builder \
     .appName("SalesETL") \
     .getOrCreate()
 
-# 1. Ler do Postgres
+# Read from PostgreSQL
 df = spark.read.format("jdbc").options(
-    url="jdbc:postgresql://postgres:5432/airflow",
+    url=jdbc_url,
     driver="org.postgresql.Driver",
     dbtable="sales",
-    user="admin",
-    password="admin"
+    user=POSTGRES_USER,
+    password=POSTGRES_PASSWORD
 ).load()
 
-# 2. Transformação
+# Transformation
 result = df.groupBy("product") \
     .agg(
         _sum(col("amount") * col("price")).alias("total_revenue")
     )
 
-# 3. Escrever resultado de volta no Postgres
+# Write back to PostgreSQL
 result.write.format("jdbc").options(
-    url="jdbc:postgresql://postgres:5432/airflow",
+    url=jdbc_url,
     driver="org.postgresql.Driver",
     dbtable="sales_summary",
-    user="admin",
-    password="admin"
+    user=POSTGRES_USER,
+    password=POSTGRES_PASSWORD
 ).mode("overwrite").save()
 
 spark.stop()
